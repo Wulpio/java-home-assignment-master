56470dd29d4c6424b6b04440a26ae644be9de6e0
DanielSan2023 <electronic.san@gmail.com>
2024-01-31 17:52:40 +0100

    created scripts for flyway test profile

---
+++ src/main/resources/db/migration/test/V2024.01.30.101__create_person_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE person
+(
+    id   INT NOT NULL,
+    name VARCHAR(255),
+    age  INT,
+    CONSTRAINT pk_person PRIMARY KEY (id)
+);
---
+++ src/main/resources/db/migration/test/V2024.01.30.102__create_department_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE department
+(
+    id   INT NOT NULL,
+    name VARCHAR(255),
+    CONSTRAINT pk_department PRIMARY KEY (id)
+);
